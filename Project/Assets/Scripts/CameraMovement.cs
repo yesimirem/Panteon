5ad8fae4e20134ff6c360d664fb1e1df0b5115a0
yesimirem <yesimiremkayaa@gmail.com>
2022-07-12 21:57:34 +0300

    Initial commit

---
+++ Project/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    [SerializeField] float smoothness = 0.3f;
+
+    private Vector3 offset;
+    private Vector3 velocity = Vector3.zero;
+
+    void Start()
+    {
+        offset = transform.position - player.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = Vector3.SmoothDamp(transform.position, player.position + offset, ref velocity, smoothness);
+    }
+}
---
+++ Project/Assets/Scripts/RotatingPlatform.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingPlatform : MonoBehaviour
+{
+    [SerializeField] float rotatingSpeed = 10f;
+    [SerializeField] Vector3 rotatingAxis;
+
+    void Update()
+    {
+        transform.Rotate(rotatingAxis, rotatingSpeed * Time.deltaTime);
+    }
+
+     private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.transform.parent = transform;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.transform.parent = null;
+        }
+    }
+}
